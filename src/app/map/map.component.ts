dc13679075894eed0e6a389faed0cc4fe052c8dc
darrellneilcouch <darrellneilcouch@gmail.com>
2018-02-08 16:43:16 -0700

    updated readme

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -5,6 +5,11 @@ import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { SelectOfficeService } from '../select-office.service';
 
+import ActionHome from 'material-ui/svg-icons/action/home';
+import ActionFlightTakeoff from 'material-ui/svg-icons/action/flight-takeoff';
+import FileCloudDownload from 'material-ui/svg-icons/file/cloud-download';
+import HardwareVideogameAsset from 'material-ui/svg-icons/hardware/videogame-asset';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -27,14 +32,20 @@ export class MapComponent implements OnInit {
 
     '#2196f3','#b6efa7','#9c27b0',
     '#18ffff','#4caf50','#ffeb3b',
-    '#f44336','#000000','#ffe95b',
+    '#f44336','#000000','#bfb0f0',
     '#ff9800','#e64a19','#212121',
     '#795548','#f50057','#0d47a1',
-    '#03a9f4','#009688','#b73768'
+    '#03a9f4','#009688','#b73768',
     '#cddc39','#1b5e20','#827717',
     '#76ff03','#e91e63','#607d8b',
-    '#e242f4','#320ea5','#37b2ac',
+    '#e242f4','#320ea5','#37b2ac'
   ];
+  posibleServices:int ;
+  dueForService:int;
+  holdInfoForTable:any = [];
+  tmpTotalCust:int = 0;
+  tmpDueForService: int = 0;
+
 
 
   //MapComponent
@@ -51,7 +62,8 @@ export class MapComponent implements OnInit {
 
   getOfficeInfo(e){
     this.getZones(e);
-    this.getCustomers(e)
+    this.getCustomers(e);
+    this.getProjectionInfo();
   }
 
   constructor(private http: Http, private selectOfficeService: SelectOfficeService) {
@@ -107,6 +119,25 @@ export class MapComponent implements OnInit {
     customerList.subscribe(data => {
       this.customers = data
     })
+
+  }
+
+  getProjectionInfo(){
+
+    this.tmpTotalCust = 20;
+    this.tmpDueForService = Math.floor((Math.random() * 20) + 1);
+
+    console.log(this.tmpTotalCust, this.tmpDueForService)
+
+      for(let i = 0; i < this.zones.length; i++){
+        this.holdInfoForTable.push({
+          zone_id: zone[i].id,
+          zone: zone[i].zone,
+          totalCustomers: this.tmpTotalCust,
+          due_for_service: this.tmpDueForService
+        })
+      }
+    console.log(this.holdInfoForTable)
   }
 
   statusFilter(e){
@@ -143,6 +174,8 @@ export class MapComponent implements OnInit {
     })
 
 
+
+
     const getMarkers = async (arr)=>{
       this.coordsArr = [];
       for(let i = 0; i < arr.length; i++){
@@ -156,9 +189,22 @@ export class MapComponent implements OnInit {
             &key=AIzaSyBPM590byZIlUBoDC3Nq5E9yxTEFdT1gH8`)
             .map((res: Response) => res.json())
           coords.subscribe(data => {
+            var zoneName:string = this.zones.filter(item => {
+              if(item.id == arr[i][j].zone_id){
+                return item.zone;
+              };
+            })
             this.coordsArr.push({
-              customer: arr[i][j  ].id,
-              customerData: this.filteredCustomers,
+              customer: arr[i][j].id,
+              first_name: arr[i][j].first_name,
+              last_name: arr[i][j].last_name,
+              address: arr[i][j].address,
+              city: arr[i][j].city,
+              state: arr[i][j].state,
+              zip_code: arr[i][j].zip_code,
+              zone_id: arr[i][j].zone_id,
+              zone: this.zoneName,
+              service_week: arr[i][j].service_week,
               latitude: data.results[0].geometry.location.lat,
               longitude: data.results[0].geometry.location.lng
             })
@@ -168,10 +214,14 @@ export class MapComponent implements OnInit {
 
 
     }
-
-    console.log(this.coorsArr)
-
     getMarkers(this.filteredCustomers)
 
+  }
+
+  handleUpdate(e){
+    console.log(e)
+    this.coordsArr = [];
+
+    // this.http.post(`${this.apiURL}customer/${id}`, e.target)
   }
 }
